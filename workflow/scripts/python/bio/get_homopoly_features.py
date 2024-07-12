e59b4bf8caaa7d9145a756e4f600a3f8129ca657
ndwarshuis <ndwar@yavin4.ch>
2024-07-12 15:40:24 -0400

    ENH use streaming for homopolymers

--- workflow/scripts/python/bio/get_homopoly_features.py
+++ workflow/scripts/python/bio/get_homopoly_features.py
@@ -1,72 +1,142 @@
+import os
+import threading as tr
+import gzip
 from pathlib import Path
-import pandas as pd
+import subprocess as sp
 import common.config as cfg
-from typing import Any, cast
-from pybedtools import BedTool as bt  # type: ignore
-from pybedtools import cleanup
-from common.tsv import write_tsv
-from common.io import setup_logging
-from common.bed import read_bed
+from typing import Any, IO
 
 
-logger = setup_logging(snakemake.log[0])  # type: ignore
+def spawn_stream(
+    cmd: list[str],
+    i: IO[bytes] | int | None = None,
+) -> tuple[sp.Popen[bytes], IO[bytes]]:
+    p = sp.Popen(cmd, stdin=i, stdout=sp.PIPE, stderr=sp.PIPE)
+    # ASSUME since we typed the inputs so that the stdin/stdout can only take
+    # file descriptors or file streams, the return for each will never be
+    # none
+    if p.stdout is None:
+        raise
+    return p, p.stdout
+
+
+def check_processes(
+    ps: list[sp.Popen[bytes] | sp.CompletedProcess[bytes]], log: Path
+) -> None:
+    some_error = False
+    # TODO make parent directory if it doesn't exist? probably won't be necessary
+    with open(log, "w") as lf:
+        for p in ps:
+            if isinstance(p, sp.CompletedProcess):
+                err = p.stderr
+            else:
+                _, err = p.communicate()  # break deadlocks if there are any
+            if p.returncode != 0:
+                some_error = True
+
+                args = p.args
+                if isinstance(args, list):
+                    cmd = " ".join(args)
+                elif isinstance(args, bytes):
+                    cmd = args.decode()
+                else:
+                    cmd = str(args)
+                if err:
+                    lf.write(f"{cmd}: {err.decode()}\n")
+                else:
+                    lf.write(f"{cmd}: return code {p.returncode}\n")
+
+    if some_error:
+        exit(1)
+
+
+# logger = setup_logging(snakemake.log[0])  # type: ignore
 
 # temporary columns used for dataframe processing
-BASE_COL = "_base"
-PFCT_LEN_COL = "_perfect_length"
 
 SLOP = 1
 
 
-def read_input(path: Path) -> pd.DataFrame:
-    logger.info("Reading dataframe from %s", path)
-    return read_bed(path, more={3: BASE_COL})
-
-
 def merge_base(
     config: cfg.StratoMod,
-    df: pd.DataFrame,
+    i: Path,
+    o: Path,
+    genome: Path,
     base: cfg.Base,
-    genome: str,
-) -> pd.DataFrame:
-    logger.info("Filtering bed file for %ss", base)
-    _df = df[df[BASE_COL] == f"unit={base.value}"].drop(columns=[BASE_COL])
-    logger.info("Merging %s rows for %ss", len(_df), base)
-    # Calculate the length of each "pure" homopolymer (eg just "AAAAAAAA").
-    # Note that this is summed in the merge below, and the final length based
-    # on start/end won't necessarily be this sum because of the -d 1 parameter
-    _df[PFCT_LEN_COL] = _df[cfg.BED_END] - _df[cfg.BED_START]
-    merged = cast(
-        pd.DataFrame,
-        bt.from_dataframe(_df)
-        .merge(d=1, c=[4], o=["sum"])
-        .slop(b=SLOP, g=genome)
-        .to_dataframe(names=[*cfg.BED_COLS, PFCT_LEN_COL]),
-    )
-    # these files are huge; now that we have a dataframe, remove all the bed
-    # files from tmpfs to prevent a run on downloadmoreram.com
-    cleanup()
-
+    log: Path,
+) -> None:
+    baseval = f"unit={base.value}".encode()
     hgroup = config.feature_definitions.homopolymers
-
     length_col = hgroup.fmt_name(base, lambda x: x.len)
     frac_col = hgroup.fmt_name(base, lambda x: x.imp_frac)
+    cols = [x.encode() for x in [*cfg.BED_COLS, length_col, frac_col]]
 
-    merged[length_col] = merged[cfg.BED_END] - merged[cfg.BED_START] - SLOP * 2
-    merged[frac_col] = 1 - (merged[PFCT_LEN_COL] / merged[length_col])
-    return merged.drop(columns=[PFCT_LEN_COL])
+    mergecmd = ["mergeBed", "-i", "stdin", "-d", "1", "-c", "4", "-o", "sum"]
+    slopcmd = ["slopBed", "-i", "stdin", "-b", str(SLOP), "-g", str(genome)]
+
+    # ASSUME input is already sorted
+    with gzip.open(i, "r") as ih, gzip.open(o, "w") as oh:
+        # write header to output first
+        oh.write(b"\t".join(cols) + b"\n")
+
+        r, w = os.pipe()
+        r0 = os.fdopen(r, "rb")
+        w0 = os.fdopen(w, "wb")
+
+        try:
+            # 1. Filter stream for lines that match 'base' and calculate length.
+            # Resulting stream will have 4 columns (chrom, start, end, length)
+            def filter_base() -> None:
+                try:
+                    for x in ih:
+                        s = x.rstrip().split(b"\t")
+                        if baseval == s[3]:
+                            start = int(s[1])
+                            end = int(s[2])
+                            newline = [*s[:3], str(end - start).encode()]
+                            w0.write(b"\t".join(newline) + b"\n")
+                except Exception:
+                    r0.close()
+                finally:
+                    w0.close()
+
+            t1 = tr.Thread(target=filter_base)
+            t1.start()
+
+            # 2. Merge this stream using mergeBed and sum the 4th column
+            p1, r1 = spawn_stream(mergecmd, r0)
+
+            # 3. Add slop to stream
+            p2, r2 = spawn_stream(slopcmd, r1)
+
+            # 4. Compute real length of (possibly imperfect) homopolymer and
+            # imperfect fraction. Write the result. Assume input has 4 columns
+            # (chrom, start, end, sum_of_merged_length_before_slop)
+            for x in r2:
+                s = x.rstrip().split(b"\t")
+                start = int(s[1])
+                end = int(s[2])
+                perfect_length = int(s[3])
+                real_length = end - start - SLOP * 2
+                frac = 1 - perfect_length / real_length
+                newline = [*s[:3], str(real_length).encode(), str(frac).encode()]
+                oh.write(b"\t".join(newline) + b"\n")
+
+            check_processes([p1, p2], log)
+        finally:
+            r0.close()
+            w0.close()
 
 
 def main(smk: Any, config: cfg.StratoMod) -> None:
-    # ASSUME this file is already sorted
-    simreps = read_input(smk.input["bed"][0])
-    merged = merge_base(
+    merge_base(
         config,
-        simreps,
+        Path(smk.input["bed"][0]),
+        Path(smk.output[0]),
+        Path(smk.input["genome"][0]),
         cfg.Base(smk.wildcards["base"]),
-        smk.input["genome"][0],
+        Path(smk.log[0]),
     )
-    write_tsv(smk.output[0], merged, header=True)
 
 
 main(snakemake, snakemake.config)  # type: ignore
